38d6ca97dd2cabb60880d5d7e65a2b5b4783aa7c
Yash2402 <mrsmeenakumaris@gmail.com>
2024-05-13 17:14:10 +0530

    added seperator to footer

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/pages/index_BVnSCwtN.mjs
@@ -0,0 +1,100 @@
+/* empty css                          */
+import { e as createComponent, r as renderTemplate, m as maybeRenderHead, j as renderComponent, g as addAttribute, i as renderHead, h as createAstro } from '../astro_BGJ4o0zo.mjs';
+import 'kleur/colors';
+import 'html-escaper';
+import { $ as $$Image, y as yash } from './about_DblQlcfd.mjs';
+/* empty css                          */
+import 'clsx';
+/* empty css                          */
+
+const navLinks = [
+  {
+    title: "about",
+    href: "/about/"
+  },
+  {
+    title: "blogs",
+    href: "/blogs/"
+  },
+  {
+    title: "projects",
+    href: "/projects/"
+  },
+  {
+    title: "renders",
+    href: "/renders/"
+  },
+  {
+    title: "links",
+    href: "/links/"
+  }
+];
+
+const LM_to_DM = new Proxy({"src":"/_astro/LM to DM.kjIJzSBY.svg","width":33,"height":33,"format":"svg"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "/Users/yashsharma/dev/webdev/yash/src/imgs/LM to DM.svg";
+							}
+							
+							return target[name];
+						}
+					});
+
+const $$Nav = createComponent(($$result, $$props, $$slots) => {
+  return renderTemplate`${maybeRenderHead()}<header class="mt-[45px] mx-[75px]" data-astro-cid-dmqpwcec> <nav class="bg-[#D9D9D9] rounded-[25px] px-[25px] py-4 flex flex-row items-center justify-between" data-astro-cid-dmqpwcec> ${renderComponent($$result, "Image", $$Image, { "src": yash, "alt": "Personal Logo", "height": 50, "width": 94, "data-astro-cid-dmqpwcec": true })} <div class="flex flex-row items-center space-x-[45px]" data-astro-cid-dmqpwcec> ${navLinks.map((navLink) => renderTemplate`<li class="list-none" data-astro-cid-dmqpwcec> <a class="text-[#67a0b5] hover:text-[#007099]"${addAttribute(navLink.href, "href")} data-astro-cid-dmqpwcec> ${navLink.title} </a> </li>`)} <button class="mt-[7px]" data-astro-cid-dmqpwcec>${renderComponent($$result, "Image", $$Image, { "src": LM_to_DM, "height": 30, "width": 30, "alt": "Light mode to Dark Mode switcher", "data-astro-cid-dmqpwcec": true })}</button> </div> </nav> </header> `;
+}, "/Users/yashsharma/dev/webdev/yash/src/components/Nav.astro", void 0);
+
+const footerLinks = [
+  {
+    title: "E-mail",
+    href: "mailto:yashsharma4583@gmail.com"
+  },
+  {
+    title: "Art Station",
+    href: "https://www.artstation.com/yash2402"
+  },
+  {
+    title: "Github",
+    href: "https://github.com/Yash2402"
+  },
+  {
+    title: "Youtube",
+    href: "https://www.youtube.com/@YashSharma-qj8pd"
+  }
+];
+
+const $$Footer = createComponent(($$result, $$props, $$slots) => {
+  return renderTemplate`${maybeRenderHead()}<div class="mb-[45px] mt-[45px] mx-[75px]" data-astro-cid-sz7xmlte> <nav class="flex flex-row items-center justify-between" data-astro-cid-sz7xmlte> <p class="mx-[35px]" data-astro-cid-sz7xmlte>designed and developed by <span data-astro-cid-sz7xmlte>Yash Sharma</span></p> <div class="flex flex-row items-center space-x-[30px] mx-[35px]" data-astro-cid-sz7xmlte> ${footerLinks.map((footerLink) => renderTemplate`<li class="list-none" data-astro-cid-sz7xmlte> <a class="text-[#000000]"${addAttribute(footerLink.href, "href")} data-astro-cid-sz7xmlte> ${footerLink.title} </a> </li>`)} </div> </nav> </div> `;
+}, "/Users/yashsharma/dev/webdev/yash/src/components/Footer.astro", void 0);
+
+const display_img = new Proxy({"src":"/_astro/example.CnPgWQtE.png","width":1132,"height":364,"format":"png"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "/Users/yashsharma/dev/webdev/yash/src/imgs/example.png";
+							}
+							
+							return target[name];
+						}
+					});
+
+const $$Astro = createAstro();
+const $$Index = createComponent(($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Index;
+  return renderTemplate`<html lang="en" data-astro-cid-j7pv25f6> <head><meta charset="utf-8"><link rel="icon" type="image/svg+xml" href="y.svg"><meta name="viewport" content="width=device-width"><meta name="generator"${addAttribute(Astro2.generator, "content")}><title>yash.</title>${renderHead()}</head> <body class="bg-[#EEEEEE]" data-astro-cid-j7pv25f6> ${renderComponent($$result, "Nav", $$Nav, { "data-astro-cid-j7pv25f6": true })} <main class="flex flex-col items-center justify-start mx-[120px] my-[84px]" data-astro-cid-j7pv25f6> <div class="flex flex-row w-full justify-between items-center" data-astro-cid-j7pv25f6> <p class="left" data-astro-cid-j7pv25f6>
+Hey, I am <br data-astro-cid-j7pv25f6>
+Yash Sharma.
+</p> <p class="right" data-astro-cid-j7pv25f6>
+I am currently in university as a sophomore studying <br data-astro-cid-j7pv25f6> <span data-astro-cid-j7pv25f6>Mechanical Engineering.</span> </p> </div> </main> <main class="flex flex-col items-center justify-start mx-[160px] mt-[100px] mb-[65px]" data-astro-cid-j7pv25f6> ${renderComponent($$result, "Image", $$Image, { "class": "bg-[#D9D9D9] rounded-[16px] px-[10px] py-[10px]", "src": display_img, "height": 200, "width": 1e3, "alt": "Display Item", "data-astro-cid-j7pv25f6": true })} </main> ${renderComponent($$result, "Footer", $$Footer, { "data-astro-cid-j7pv25f6": true })} </body></html>`;
+}, "/Users/yashsharma/dev/webdev/yash/src/pages/index.astro", void 0);
+
+const $$file = "/Users/yashsharma/dev/webdev/yash/src/pages/index.astro";
+const $$url = "";
+
+export { $$Index as default, $$file as file, $$url as url };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/manifest_DmMbKv35.mjs
@@ -0,0 +1,205 @@
+import 'cookie';
+import { bold, red, yellow, dim, blue } from 'kleur/colors';
+import 'html-escaper';
+import 'clsx';
+import './chunks/astro_BGJ4o0zo.mjs';
+import { compile } from 'path-to-regexp';
+
+const dateTimeFormat = new Intl.DateTimeFormat([], {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: false
+});
+const levels = {
+  debug: 20,
+  info: 30,
+  warn: 40,
+  error: 50,
+  silent: 90
+};
+function log(opts, level, label, message, newLine = true) {
+  const logLevel = opts.level;
+  const dest = opts.dest;
+  const event = {
+    label,
+    level,
+    message,
+    newLine
+  };
+  if (!isLogLevelEnabled(logLevel, level)) {
+    return;
+  }
+  dest.write(event);
+}
+function isLogLevelEnabled(configuredLogLevel, level) {
+  return levels[configuredLogLevel] <= levels[level];
+}
+function info(opts, label, message, newLine = true) {
+  return log(opts, "info", label, message, newLine);
+}
+function warn(opts, label, message, newLine = true) {
+  return log(opts, "warn", label, message, newLine);
+}
+function error(opts, label, message, newLine = true) {
+  return log(opts, "error", label, message, newLine);
+}
+function debug(...args) {
+  if ("_astroGlobalDebug" in globalThis) {
+    globalThis._astroGlobalDebug(...args);
+  }
+}
+function getEventPrefix({ level, label }) {
+  const timestamp = `${dateTimeFormat.format(/* @__PURE__ */ new Date())}`;
+  const prefix = [];
+  if (level === "error" || level === "warn") {
+    prefix.push(bold(timestamp));
+    prefix.push(`[${level.toUpperCase()}]`);
+  } else {
+    prefix.push(timestamp);
+  }
+  if (label) {
+    prefix.push(`[${label}]`);
+  }
+  if (level === "error") {
+    return red(prefix.join(" "));
+  }
+  if (level === "warn") {
+    return yellow(prefix.join(" "));
+  }
+  if (prefix.length === 1) {
+    return dim(prefix[0]);
+  }
+  return dim(prefix[0]) + " " + blue(prefix.splice(1).join(" "));
+}
+if (typeof process !== "undefined") {
+  let proc = process;
+  if ("argv" in proc && Array.isArray(proc.argv)) {
+    if (proc.argv.includes("--verbose")) ; else if (proc.argv.includes("--silent")) ; else ;
+  }
+}
+class Logger {
+  options;
+  constructor(options) {
+    this.options = options;
+  }
+  info(label, message, newLine = true) {
+    info(this.options, label, message, newLine);
+  }
+  warn(label, message, newLine = true) {
+    warn(this.options, label, message, newLine);
+  }
+  error(label, message, newLine = true) {
+    error(this.options, label, message, newLine);
+  }
+  debug(label, ...messages) {
+    debug(label, ...messages);
+  }
+  level() {
+    return this.options.level;
+  }
+  forkIntegrationLogger(label) {
+    return new AstroIntegrationLogger(this.options, label);
+  }
+}
+class AstroIntegrationLogger {
+  options;
+  label;
+  constructor(logging, label) {
+    this.options = logging;
+    this.label = label;
+  }
+  /**
+   * Creates a new logger instance with a new label, but the same log options.
+   */
+  fork(label) {
+    return new AstroIntegrationLogger(this.options, label);
+  }
+  info(message) {
+    info(this.options, this.label, message);
+  }
+  warn(message) {
+    warn(this.options, this.label, message);
+  }
+  error(message) {
+    error(this.options, this.label, message);
+  }
+  debug(message) {
+    debug(this.label, message);
+  }
+}
+
+function getRouteGenerator(segments, addTrailingSlash) {
+  const template = segments.map((segment) => {
+    return "/" + segment.map((part) => {
+      if (part.spread) {
+        return `:${part.content.slice(3)}(.*)?`;
+      } else if (part.dynamic) {
+        return `:${part.content}`;
+      } else {
+        return part.content.normalize().replace(/\?/g, "%3F").replace(/#/g, "%23").replace(/%5B/g, "[").replace(/%5D/g, "]").replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+      }
+    }).join("");
+  }).join("");
+  let trailing = "";
+  if (addTrailingSlash === "always" && segments.length) {
+    trailing = "/";
+  }
+  const toPath = compile(template + trailing);
+  return (params) => {
+    const path = toPath(params);
+    return path || "/";
+  };
+}
+
+function deserializeRouteData(rawRouteData) {
+  return {
+    route: rawRouteData.route,
+    type: rawRouteData.type,
+    pattern: new RegExp(rawRouteData.pattern),
+    params: rawRouteData.params,
+    component: rawRouteData.component,
+    generate: getRouteGenerator(rawRouteData.segments, rawRouteData._meta.trailingSlash),
+    pathname: rawRouteData.pathname || void 0,
+    segments: rawRouteData.segments,
+    prerender: rawRouteData.prerender,
+    redirect: rawRouteData.redirect,
+    redirectRoute: rawRouteData.redirectRoute ? deserializeRouteData(rawRouteData.redirectRoute) : void 0,
+    fallbackRoutes: rawRouteData.fallbackRoutes.map((fallback) => {
+      return deserializeRouteData(fallback);
+    }),
+    isIndex: rawRouteData.isIndex
+  };
+}
+
+function deserializeManifest(serializedManifest) {
+  const routes = [];
+  for (const serializedRoute of serializedManifest.routes) {
+    routes.push({
+      ...serializedRoute,
+      routeData: deserializeRouteData(serializedRoute.routeData)
+    });
+    const route = serializedRoute;
+    route.routeData = deserializeRouteData(serializedRoute.routeData);
+  }
+  const assets = new Set(serializedManifest.assets);
+  const componentMetadata = new Map(serializedManifest.componentMetadata);
+  const inlinedScripts = new Map(serializedManifest.inlinedScripts);
+  const clientDirectives = new Map(serializedManifest.clientDirectives);
+  return {
+    // in case user middleware exists, this no-op middleware will be reassigned (see plugin-ssr.ts)
+    middleware(_, next) {
+      return next();
+    },
+    ...serializedManifest,
+    assets,
+    componentMetadata,
+    inlinedScripts,
+    clientDirectives,
+    routes
+  };
+}
+
+const manifest = deserializeManifest({"adapterName":"@astrojs/vercel/serverless","routes":[{"file":"","links":[],"scripts":[],"styles":[],"routeData":{"type":"endpoint","isIndex":false,"route":"/_image","pattern":"^\\/_image$","segments":[[{"content":"_image","dynamic":false,"spread":false}]],"params":[],"component":"node_modules/astro/dist/assets/endpoint/generic.js","pathname":"/_image","prerender":false,"fallbackRoutes":[],"_meta":{"trailingSlash":"ignore"}}},{"file":"","links":[],"scripts":[],"styles":[{"type":"external","src":"/_astro/about.BuxKIoPY.css"},{"type":"inline","content":"@font-face{font-family:Playfair Display Variable;font-style:normal;font-display:swap;font-weight:400 900;src:url(/_astro/playfair-display-cyrillic-wght-normal.Dr2SnCEI.woff2) format(\"woff2-variations\");unicode-range:U+0301,U+0400-045F,U+0490-0491,U+04B0-04B1,U+2116}@font-face{font-family:Playfair Display Variable;font-style:normal;font-display:swap;font-weight:400 900;src:url(/_astro/playfair-display-vietnamese-wght-normal.DQ2yjD0Y.woff2) format(\"woff2-variations\");unicode-range:U+0102-0103,U+0110-0111,U+0128-0129,U+0168-0169,U+01A0-01A1,U+01AF-01B0,U+0300-0301,U+0303-0304,U+0308-0309,U+0323,U+0329,U+1EA0-1EF9,U+20AB}@font-face{font-family:Playfair Display Variable;font-style:normal;font-display:swap;font-weight:400 900;src:url(/_astro/playfair-display-latin-ext-wght-normal.CR05oQ3b.woff2) format(\"woff2-variations\");unicode-range:U+0100-02AF,U+0304,U+0308,U+0329,U+1E00-1E9F,U+1EF2-1EFF,U+2020,U+20A0-20AB,U+20AD-20C0,U+2113,U+2C60-2C7F,U+A720-A7FF}@font-face{font-family:Playfair Display Variable;font-style:normal;font-display:swap;font-weight:400 900;src:url(/_astro/playfair-display-latin-wght-normal.BGlevZWC.woff2) format(\"woff2-variations\");unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD}\ndiv[data-astro-cid-kh7btl4r]{color:#000;font-family:Playfair Display;font-size:40px;font-style:normal;font-weight:100;line-height:normal;letter-spacing:-1.05px}\n"}],"routeData":{"route":"/about","isIndex":false,"type":"page","pattern":"^\\/about\\/?$","segments":[[{"content":"about","dynamic":false,"spread":false}]],"params":[],"component":"src/pages/about.astro","pathname":"/about","prerender":false,"fallbackRoutes":[],"_meta":{"trailingSlash":"ignore"}}},{"file":"","links":[],"scripts":[],"styles":[{"type":"external","src":"/_astro/about.BuxKIoPY.css"}],"routeData":{"route":"/blogs","isIndex":false,"type":"page","pattern":"^\\/blogs\\/?$","segments":[[{"content":"blogs","dynamic":false,"spread":false}]],"params":[],"component":"src/pages/blogs.astro","pathname":"/blogs","prerender":false,"fallbackRoutes":[],"_meta":{"trailingSlash":"ignore"}}},{"file":"","links":[],"scripts":[],"styles":[{"type":"external","src":"/_astro/about.BuxKIoPY.css"},{"type":"inline","content":"@font-face{font-family:Playfair Display Variable;font-style:normal;font-display:swap;font-weight:400 900;src:url(/_astro/playfair-display-cyrillic-wght-normal.Dr2SnCEI.woff2) format(\"woff2-variations\");unicode-range:U+0301,U+0400-045F,U+0490-0491,U+04B0-04B1,U+2116}@font-face{font-family:Playfair Display Variable;font-style:normal;font-display:swap;font-weight:400 900;src:url(/_astro/playfair-display-vietnamese-wght-normal.DQ2yjD0Y.woff2) format(\"woff2-variations\");unicode-range:U+0102-0103,U+0110-0111,U+0128-0129,U+0168-0169,U+01A0-01A1,U+01AF-01B0,U+0300-0301,U+0303-0304,U+0308-0309,U+0323,U+0329,U+1EA0-1EF9,U+20AB}@font-face{font-family:Playfair Display Variable;font-style:normal;font-display:swap;font-weight:400 900;src:url(/_astro/playfair-display-latin-ext-wght-normal.CR05oQ3b.woff2) format(\"woff2-variations\");unicode-range:U+0100-02AF,U+0304,U+0308,U+0329,U+1E00-1E9F,U+1EF2-1EFF,U+2020,U+20A0-20AB,U+20AD-20C0,U+2113,U+2C60-2C7F,U+A720-A7FF}@font-face{font-family:Playfair Display Variable;font-style:normal;font-display:swap;font-weight:400 900;src:url(/_astro/playfair-display-latin-wght-normal.BGlevZWC.woff2) format(\"woff2-variations\");unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD}\n"},{"type":"external","src":"/_astro/index.BNzG2n3y.css"}],"routeData":{"route":"/","isIndex":true,"type":"page","pattern":"^\\/$","segments":[],"params":[],"component":"src/pages/index.astro","pathname":"/","prerender":false,"fallbackRoutes":[],"_meta":{"trailingSlash":"ignore"}}}],"base":"/","trailingSlash":"ignore","compressHTML":true,"componentMetadata":[["/Users/yashsharma/dev/webdev/yash/src/pages/about.astro",{"propagation":"none","containsHead":true}],["/Users/yashsharma/dev/webdev/yash/src/pages/index.astro",{"propagation":"none","containsHead":true}]],"renderers":[],"clientDirectives":[["idle","(()=>{var i=t=>{let e=async()=>{await(await t())()};\"requestIdleCallback\"in window?window.requestIdleCallback(e):setTimeout(e,200)};(self.Astro||(self.Astro={})).idle=i;window.dispatchEvent(new Event(\"astro:idle\"));})();"],["load","(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).load=e;window.dispatchEvent(new Event(\"astro:load\"));})();"],["media","(()=>{var s=(i,t)=>{let a=async()=>{await(await i())()};if(t.value){let e=matchMedia(t.value);e.matches?a():e.addEventListener(\"change\",a,{once:!0})}};(self.Astro||(self.Astro={})).media=s;window.dispatchEvent(new Event(\"astro:media\"));})();"],["only","(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).only=e;window.dispatchEvent(new Event(\"astro:only\"));})();"],["visible","(()=>{var l=(s,i,o)=>{let r=async()=>{await(await s())()},t=typeof i.value==\"object\"?i.value:void 0,c={rootMargin:t==null?void 0:t.rootMargin},n=new IntersectionObserver(e=>{for(let a of e)if(a.isIntersecting){n.disconnect(),r();break}},c);for(let e of o.children)n.observe(e)};(self.Astro||(self.Astro={})).visible=l;window.dispatchEvent(new Event(\"astro:visible\"));})();"]],"entryModules":{"\u0000@astrojs-ssr-virtual-entry":"entry.mjs","\u0000@astro-renderers":"renderers.mjs","\u0000noop-middleware":"_noop-middleware.mjs","/src/pages/blogs.astro":"chunks/pages/blogs_BHbZlwA2.mjs","/node_modules/astro/dist/assets/endpoint/generic.js":"chunks/pages/generic_DeuTtP--.mjs","/src/pages/index.astro":"chunks/pages/index_BVnSCwtN.mjs","\u0000@astrojs-manifest":"manifest_DmMbKv35.mjs","\u0000@astro-page:node_modules/astro/dist/assets/endpoint/generic@_@js":"chunks/generic_CnPWsMKP.mjs","\u0000@astro-page:src/pages/about@_@astro":"chunks/about_CvKm97zr.mjs","\u0000@astro-page:src/pages/blogs@_@astro":"chunks/blogs_Bj2szWo6.mjs","\u0000@astro-page:src/pages/index@_@astro":"chunks/index_DD806HXA.mjs","astro:scripts/before-hydration.js":""},"inlinedScripts":[],"assets":["/_astro/playfair-display-latin-ext-wght-normal.CR05oQ3b.woff2","/_astro/playfair-display-cyrillic-wght-normal.Dr2SnCEI.woff2","/_astro/playfair-display-vietnamese-wght-normal.DQ2yjD0Y.woff2","/_astro/yash.CPZ5SQHM.svg","/_astro/playfair-display-latin-wght-normal.BGlevZWC.woff2","/_astro/example.CnPgWQtE.png","/_astro/medula-one-latin-400-normal.C4HvygfD.woff2","/_astro/ibm-plex-sans-hebrew-latin-400-normal.l4hZqPPq.woff2","/_astro/ibm-plex-sans-hebrew-hebrew-400-normal.C4n6fljN.woff2","/_astro/ibm-plex-sans-hebrew-latin-ext-400-normal.BfZrEZK1.woff2","/_astro/LM to DM.kjIJzSBY.svg","/_astro/medula-one-latin-400-normal.D2ZOpJMg.woff","/_astro/ibm-plex-sans-hebrew-hebrew-400-normal.CpN4dYuG.woff","/_astro/ibm-plex-sans-hebrew-latin-ext-400-normal.CoqQy45L.woff","/_astro/ibm-plex-sans-hebrew-latin-400-normal.T6wL9k6Z.woff","/_astro/about.BuxKIoPY.css","/_astro/index.BNzG2n3y.css","/favicon.svg","/y.svg"],"buildFormat":"directory","checkOrigin":false,"rewritingEnabled":false});
+
+export { AstroIntegrationLogger as A, Logger as L, getEventPrefix as g, levels as l, manifest };
