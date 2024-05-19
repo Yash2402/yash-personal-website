c094ac9c8f894426b90292141360fe1ed36a356f
Yash2402 <mrsmeenakumaris@gmail.com>
2024-05-19 18:07:59 +0530

    added color to current nav

--- src/utils/navitems.ts
+++
@@ -1,7 +0,0 @@
-export const navItemsList={
-    "about": "ABOUT",
-    "blogs": "BLOGS", 
-    "projects": "PROJECTS", 
-    "renders": "RENDERS", 
-    "links": "LINKS", 
-}
--- src/utils/types.ts
+++
@@ -1,4 +0,0 @@
-export interface navItems {
-    title: string, 
-    href: string
-}
