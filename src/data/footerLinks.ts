d81ba685700677fb84b5d719dbe87324262563f6
Yash2402 <mrsmeenakumaris@gmail.com>
2025-03-06 03:11:51 +0530

    footer updated

--- src/data/footerLinks.ts
+++ src/data/footerLinks.ts
@@ -1,23 +1,27 @@
 interface footerLinks {
-    title: string,
-    href: string,
+  title: string;
+  href: string;
 }
 
 export const footerLinks: footerLinks[] = [
-    {
-        title: "E-mail",
-        href: "mailto:yashsharma4583@gmail.com"
-    },
-    {
-        title: "Art Station",
-        href: "https://www.artstation.com/yash2402",
-    },
-    {
-        title: "Github",
-        href: "https://www.github.com/Yash2402",
-    },
-    {
-        title: "Youtube",
-        href: "https://www.youtube.com/@YashSharma-qj8pd",
-    },
-]
+  {
+    title: "E-mail",
+    href: "mailto:yashsharma4583@gmail.com",
+  },
+  {
+    title: "Art Station",
+    href: "https://www.artstation.com/yash2402",
+  },
+  {
+    title: "Github",
+    href: "https://www.github.com/Yash2402",
+  },
+  {
+    title: "Youtube",
+    href: "https://www.youtube.com/@YashSharma-qj8pd",
+  },
+  {
+    title: "Instagram",
+    href: "https://www.instagram.com/yash.sharma.2402/",
+  },
+];
