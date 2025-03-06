66a18a402651ba12e4140b9a2306325773ecc339
Yash2402 <mrsmeenakumaris@gmail.com>
2025-03-06 17:48:07 +0530

    font changed form IBM Plex Sans to Satoshi-Variable

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,8 +9,8 @@ export default {
       "LM-Theme-Toggler": "#000000",
       "LM-Background": "#eeeeee",
       "LM-Secondary": "#cccccc",
-      "LM-Nav-Unselected": "#67A0B5",
-      "LM-Nav-Selected": "#416572",
+      "LM-Nav-Unselected": "#666666",
+      "LM-Nav-Selected": "#2AA9B9",
       "LM-Title": "#000000",
       "LM-Para": "#585858",
       "LM-Title-Bar": "#E9E9E9",
@@ -21,77 +21,73 @@ export default {
       "DM-Theme-Toggler": "#EEEEEE",
       "DM-Background": "#1B1B1B",
       "DM-Secondary": "#444444",
-      "DM-Nav-Unselected": "#98C3D2",
-      "DM-Nav-Selected": "#89E1FF",
+      "DM-Nav-Unselected": "#666666",
+      "DM-Nav-Selected": "#4CCBDB",
       "DM-Nav-Hover": "#0086B7",
       "DM-Title": "#FFFFFF",
       "DM-Para": "#c4c4c4",
       "DM-Title-Bar": "#242424",
-      "DM-Link": "#85CDE6",
+      "DM-Link": "#4CCBDB",
     },
     fontFamily: {
-	    "title"  : 'Satoshi Variable', /*"ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"*/
-        "crumbs" : 'Comfortaa Variable',
-        "para"   : 'Newsreader Variable',
-        "home"   : 'Playfair Display Variable', /*"ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"*/
+      title:
+        "/fonts/Satoshi-Variable.ttf" /*"ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"*/,
+      crumbs: "Comfortaa Variable",
+      para: "Newsreader Variable",
+      home: "Playfair Display Variable" /*"ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"*/,
     },
 
     extend: {
-			spacing: {
-				// '6.5xl': '75rem',
-			},
-			typography: (theme) => ({
-				DEFALUT: {
-					css:
-					{
-						// Colours
-						'--tw-prose-body': theme('colors.LM-Para'),
-                        '--tw-prose-lead': theme('colors.LM-Title'),
-                        '--tw-prose-headings': theme('colors.LM-Title'),
-                        '--tw-prose-links': theme('colors.LM-Link'),
-                        '--tw-prose-bold': theme('colors.LM-Title'),
-                        '--tw-prose-counters': theme('colors.LM-Para'),
-                        '--tw-prose-bullets': theme('colors.LM-Para'),
-                        '--tw-prose-hr': theme('colors.LM-Para'),
-                        '--tw-prose-quotes': theme('colors.LM-Title'),
-                        '--tw-prose-quote-borders': theme('colors.LM-Secondary'),
-                        '--tw-prose-captions': theme('colors.LM-Title'),
-                        '--tw-prose-kbd': theme('colors.LM-Title'),
-                        '--tw-prose-kbd-shadows': theme('colors.LM-Title'),
-                        '--tw-prose-code': theme('colors.LM-Title'),
-                        '--tw-prose-pre-code': theme('colors.LM-Title'),
-                        '--tw-prose-pre-bg': theme('colors.LM-Title'),
-                        '--tw-prose-th-borders': theme('colors.LM-Title'),
-                        '--tw-prose-td-borders': theme('colors.LM-Title'),
-                        '--tw-prose-invert-body': theme('colors.DM-Para'),
-                        '--tw-prose-invert-headings': theme('colors.DM-Title'),
-                        '--tw-prose-invert-lead': theme('colors.DM-Title'),
-                        '--tw-prose-invert-links': theme('colors.DM-Link'),
-                        '--tw-prose-invert-bold': theme('colors.DM-Title'),
-                        '--tw-prose-invert-counters': theme('colors.DM-Para'),
-                        '--tw-prose-invert-bullets': theme('colors.DM-Para'),
-                        '--tw-prose-invert-hr': theme('colors.DM-Para'),
-                        '--tw-prose-invert-quotes': theme('colors.DM-Title'),
-                        '--tw-prose-invert-quote-borders': theme('colors.DM-Title'),
-                        '--tw-prose-invert-captions': theme('colors.DM-Title'),
-                        '--tw-prose-invert-kbd': theme('colors.DM-Title'),
-                        '--tw-prose-invert-kbd-shadows': theme('colors.DM-Title'),
-                        '--tw-prose-invert-code': theme('colors.DM-Title'),
-                        '--tw-prose-invert-pre-code': theme('colors.DM-Title'),
-                        '--tw-prose-invert-th-borders': theme('colors.DM-Title'),
-                        '--tw-prose-invert-td-borders': theme('colors.DM-Title'),
-					}
-
-				}
-			}),
-		},
+      spacing: {
+        // '6.5xl': '75rem',
+      },
+      typography: (theme) => ({
+        DEFALUT: {
+          css: {
+            // Colours
+            "--tw-prose-body": theme("colors.LM-Para"),
+            "--tw-prose-lead": theme("colors.LM-Title"),
+            "--tw-prose-headings": theme("colors.LM-Title"),
+            "--tw-prose-links": theme("colors.LM-Link"),
+            "--tw-prose-bold": theme("colors.LM-Title"),
+            "--tw-prose-counters": theme("colors.LM-Para"),
+            "--tw-prose-bullets": theme("colors.LM-Para"),
+            "--tw-prose-hr": theme("colors.LM-Para"),
+            "--tw-prose-quotes": theme("colors.LM-Title"),
+            "--tw-prose-quote-borders": theme("colors.LM-Secondary"),
+            "--tw-prose-captions": theme("colors.LM-Title"),
+            "--tw-prose-kbd": theme("colors.LM-Title"),
+            "--tw-prose-kbd-shadows": theme("colors.LM-Title"),
+            "--tw-prose-code": theme("colors.LM-Title"),
+            "--tw-prose-pre-code": theme("colors.LM-Title"),
+            "--tw-prose-pre-bg": theme("colors.LM-Title"),
+            "--tw-prose-th-borders": theme("colors.LM-Title"),
+            "--tw-prose-td-borders": theme("colors.LM-Title"),
+            "--tw-prose-invert-body": theme("colors.DM-Para"),
+            "--tw-prose-invert-headings": theme("colors.DM-Title"),
+            "--tw-prose-invert-lead": theme("colors.DM-Title"),
+            "--tw-prose-invert-links": theme("colors.DM-Link"),
+            "--tw-prose-invert-bold": theme("colors.DM-Title"),
+            "--tw-prose-invert-counters": theme("colors.DM-Para"),
+            "--tw-prose-invert-bullets": theme("colors.DM-Para"),
+            "--tw-prose-invert-hr": theme("colors.DM-Para"),
+            "--tw-prose-invert-quotes": theme("colors.DM-Title"),
+            "--tw-prose-invert-quote-borders": theme("colors.DM-Title"),
+            "--tw-prose-invert-captions": theme("colors.DM-Title"),
+            "--tw-prose-invert-kbd": theme("colors.DM-Title"),
+            "--tw-prose-invert-kbd-shadows": theme("colors.DM-Title"),
+            "--tw-prose-invert-code": theme("colors.DM-Title"),
+            "--tw-prose-invert-pre-code": theme("colors.DM-Title"),
+            "--tw-prose-invert-th-borders": theme("colors.DM-Title"),
+            "--tw-prose-invert-td-borders": theme("colors.DM-Title"),
+          },
+        },
+      }),
+    },
   },
-  plugins: [
-    'daisyui',
-    '@tailwindcss/typography',
-   ],
-   daisyui: {
-    themes:[], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"] // name of one of the included themes for dark mode
+  plugins: ["daisyui", "@tailwindcss/typography"],
+  daisyui: {
+    themes: [], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"] // name of one of the included themes for dark mode
     base: false, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: false, // adds responsive and modifier utility classes
@@ -99,6 +95,5 @@ export default {
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
     themeRoot: "*", // The element that receives theme color CSS variables
   },
-   darkMode: ['selector', '[data-theme="dark"]'],
-   
+  darkMode: ["selector", '[data-theme="dark"]'],
 };
