e92035521c103c29643fbba584f9cc213b003e10
Yash2402 <mrsmeenakumaris@gmail.com>
2025-03-09 03:15:47 +0530

    misc changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,30 +1,36 @@
 import { z, defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
 
 const blogCollection = defineCollection({
-    type: "content", schema: ({ image }) => z.object({
-        title: z.string(),
-        author: z.string(),
-        pubDate: z.date(),
-        description: z.string(),
-        banner: z.object({ url: image(), alt: z.string() }).optional(),
-        tags: z.array(z.string()),
-        postNo: z.number(),
-    })
-})
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      author: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      banner: z.object({ url: image(), alt: z.string() }).optional(),
+      tags: z.array(z.string()),
+      postNo: z.number(),
+    }),
+});
 
 const projectCollection = defineCollection({
-    type: "content", schema: ({ image }) => z.object({
-        title: z.string(),
-        pubDate: z.date(),
-        description: z.string(),
-        banner: z.object({ url: image(), alt: z.string() }).optional(),
-        resources: z.array(z.object({ url: z.string(), name: z.string() })).optional(),
-        githubLink: z.string(),
-        postNo: z.number(),
-    })
-})
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      banner: z.object({ url: image(), alt: z.string() }).optional(),
+      resources: z
+        .array(z.object({ url: z.string(), name: z.string() }))
+        .optional(),
+      githubLink: z.string(),
+      postNo: z.number(),
+    }),
+});
 
 export const collections = {
-    'blogs': blogCollection,
-    'projects': projectCollection,
+  blogs: blogCollection,
+  projects: projectCollection,
 };
