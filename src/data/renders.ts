04f9696e8ba41dad4d1995a3278dcf2760621ec4
Yash2402 <mrsmeenakumaris@gmail.com>
2025-03-17 01:49:24 +0530

    added: more renders to render page

--- src/data/renders.ts
+++ src/data/renders.ts
@@ -2,6 +2,11 @@ import diamond from "/src/imgs/renders/diamond.png";
 import moon from "../imgs/renders/moon.png";
 import thevoid from "../imgs/renders/thevoid.png";
 import golf from "../imgs/renders/golf_square.png";
+import chair from "../imgs/renders/chair.png";
+import mcdonalds from "../imgs/renders/McDonald-Final.png";
+import ring from "../imgs/renders/ring.png";
+import peace from "../imgs/renders/peace.jpeg";
+import chrysalis from "../imgs/renders/Chrysalis.png";
 
 interface renders {
   title: string;
@@ -16,27 +21,56 @@ export const renders: renders[] = [
     href: diamond,
     pubDate: new Date("2024-02-24"),
     description:
-      "A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. ",
+      "Truth, like light through a prism, is ever-shifting—revealed only in fragments and reflections.",
+  },
+  {
+    title: "Chair",
+    href: chair,
+    pubDate: new Date("2024-04-10"),
+    description: "A simple fucking chair.",
+  },
+  {
+    title: "Ring",
+    href: ring,
+    pubDate: new Date("2024-05-02"),
+    description: "A ring that is not a ring.",
   },
   {
     title: "The Void",
     href: thevoid,
     pubDate: new Date("2024-05-08"),
-    description:
-      "A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. ",
-  },
-  {
-    title: "Moon",
-    href: moon,
-    pubDate: new Date("2024-09-11"),
-    description:
-      "A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. ",
+    description: "Me staring into the abyss, the abyss staring back.",
   },
   {
     title: "Golf",
     href: golf,
     pubDate: new Date("2024-05-14"),
     description:
-      "A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. ",
+      "A golf ball in a golf course full of flowers and ready to be hit.",
+  },
+  {
+    title: "PEACE",
+    href: peace,
+    pubDate: new Date("2024-05-28"),
+    description:
+      "Me: I want PEACE in the world. I want PEACE in the world. I want PEACE in the world.",
+  },
+  {
+    title: "McDonalds",
+    href: mcdonalds,
+    pubDate: new Date("2024-07-14"),
+    description: "The McChair—designed for those who shit everytime they sit",
+  },
+  {
+    title: "Moon",
+    href: moon,
+    pubDate: new Date("2024-09-11"),
+    description: "Not the moon gatekeeping its full potential.",
+  },
+  {
+    title: "Chrysalis",
+    href: chrysalis,
+    pubDate: new Date("2024-05-23"),
+    description: "Just a shiny shape thingy. something special",
   },
 ];
